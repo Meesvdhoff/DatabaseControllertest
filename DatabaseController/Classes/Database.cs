aaf47d3c84a5508a0cfbfa24e33daa1adf5434ec
Mees von den Hoff <PS156949@summacollege.nl>
2017-11-29 15:18:47 +0100

    Add project files.

---
+++ DatabaseController/Classes/Database.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseController.Classes
+{
+
+
+    class Database
+    {
+
+        private MySqlConnection conn;
+
+
+
+
+
+        public void Connect()
+        {
+
+            string connectionString = "SERVER=localhost;DATABASE=databasecontroller;UID=root;PASSWORD=;";
+
+            conn = new MySqlConnection(connectionString);
+
+            conn.Open();
+        }
+
+
+        public void CheckConn()
+        {
+            if (conn != null && conn.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Something went wrong with the connection!");
+            }
+        }
+
+
+        public void Login(string User, string Pass)
+        {
+            string queryLogin = "SELECT `username`, `password` FROM `login` WHERE `Username` = '" + User + "' AND `Password` = '" + Pass + "'";
+
+            MySqlCommand command = new MySqlCommand(queryLogin, conn);
+            command.Parameters.AddWithValue("@user", User);
+            command.Parameters.AddWithValue("@pass", Pass);
+
+            command.ExecuteNonQuery();
+
+            
+        }
+
+        public void Register ( string User, string Pass)
+        {
+            string passwordhashed = BCrypt.Net.BCrypt.HashPassword(Pass,BCrypt.Net.BCrypt.GenerateSalt());
+            string queryRegister = "INSERT INTO `login`(`Username`, `Password`) VALUES (@user,@pass)";
+
+            MySqlCommand command = new MySqlCommand(queryRegister, conn);
+            command.Parameters.AddWithValue("@user", User);
+            command.Parameters.AddWithValue("@pass", passwordhashed);
+        }
+
+        public void Showdatabases()
+        {
+            string queryShowdatabases = "Show databases";
+
+            MySqlCommand command = new MySqlCommand(queryShowdatabases, conn);
+
+            command.ExecuteNonQuery();
+
+           
+        }
+    }
+}
+
---
+++ DatabaseController/Login.xaml.cs
@@ -0,0 +1,59 @@
+﻿using DatabaseController.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DatabaseController
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        Database db = new Database();
+
+
+        public Login()
+        {
+            InitializeComponent();
+            db.Connect();
+            db.CheckConn();
+
+        }
+
+
+
+        private void CheckAccount()
+        {
+   
+        }
+
+        private void btRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string User = tbLogin.Text;
+            string Pass = pbPass.Password;
+            db.Register(User,Pass);
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string User = tbLogin.Text;
+            string Pass = pbPass.Password;
+            db.Login(User,Pass);
+
+            this.Hide();
+            MainWindow controller = new MainWindow();
+            controller.ShowDialog();
+        }
+    }
+}
